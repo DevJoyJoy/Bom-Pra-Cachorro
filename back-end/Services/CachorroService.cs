c12ba29ea9fe961a9ba2d31e0f84cf9758ef612f
Jhenie <highhalma@gmail.com>
2026-03-16 20:54:33 -0300

    service nao implementado, instanciado

--- back-end/Services/CachorroService.cs
+++ back-end/Services/CachorroService.cs
@@ -2,5 +2,28 @@ namespace BomPraCachorro.Services;
 
 public class CachorroService : IPetService
 {
-    
+    public async Task<Cachorro> GetCachorro (int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Cachorro>> GetAllCachorros()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Cachorro> AddCachorro (Cachorro dog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Cachorro> UpdateCachorro (Cachorro dog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteCachorro (int id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- back-end/Services/IPetService.cs
+++ back-end/Services/IPetService.cs
@@ -4,6 +4,7 @@ using BomPraCachorro.Models;
 public interface IPetService
 {
     Task<Cachorro> GetCachorro (int id);
+    Task<List<Cachorro>> GetAllCachorros();
     Task<Cachorro> AddCachorro (Cachorro dog);
     Task<Cachorro> UpdateCachorro (Cachorro dog);
     Task<bool> DeleteCachorro (int id);
