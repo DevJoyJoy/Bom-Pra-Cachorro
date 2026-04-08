3e0381b1dbc80bf921c3a6f8f0c200b4685aaf0a
Jhenie <highhalma@gmail.com>
2026-04-08 16:08:20 -0300

    endpoints adicionados, program.cs atualizado

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bfc43087a6042aedd11e80bb8d54c7d5e48370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38acf6f52674d115b24f1c063424a450cc67d8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
