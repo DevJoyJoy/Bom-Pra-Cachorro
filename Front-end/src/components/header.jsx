1b8cb87ddb6b0e6009c2d17bd36464e437997c3e
taykas <thaynaschaeffer@gmail.com>
2026-04-08 23:54:16 -0300

    doing a little responsive thing, finishing the learn to help page

--- Front-end/src/components/header.jsx
+++ Front-end/src/components/header.jsx
@@ -1,21 +1,47 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Header = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
 
     return(
-        <>
-        <div className="h-[9vh] w-full bg-[#ff66c4] flex justify-between items-center text-center text-white text-4xl font-arturo p-[1%]">
-           <section className="flex gap-4 text-center items-center justify-center ">
-               <img onClick={() => navigate("/")} src="../public/icon paw.png" alt="" className="w-[4vw] mb-[2]"/>
-               <h1>Bom pra Cachorro</h1>
-           </section>
-           <section className="flex gap-4 text-lg">
-            <Link to="/Pets"> Adote </Link>
-            <Link to="/Help">Como Ajudar</Link>
-            <Link to="/LearnMore"> Saiba Mais </Link>
-           </section>
+        <div className="w-full bg-[#ff66c4] text-white">
+
+            <div className="h-[9vh] flex justify-between items-center px-5 md:px-10">
+                <section 
+                    className="flex gap-2 items-center cursor-pointer"
+                    onClick={() => navigate("/")}
+                >
+                    <img src="/icon paw.png" className="w-[10vw] md:w-[4vw]" />
+                    <h1 className="text-lg md:text-2xl lg:text-3xl font-arturo">
+                        Bom pra Cachorro
+                    </h1>
+                </section>
+
+                {/* MENU DESKTOP */}
+                <section className="hidden md:flex gap-6 text-lg">
+                    <Link to="/Pets">Adote</Link>
+                    <Link to="/Help">Como Ajudar</Link>
+                    <Link to="/LearnMore">Saiba Mais</Link>
+                </section>
+
+                {/* BOTÃO MOBILE */}
+                <div className="md:hidden text-3xl cursor-pointer"onClick={() => setOpen(!open)}
+                >
+                    ☰
+                </div>
+            </div>
+
+            {/* MENU MOBILE */}
+            {open && (
+                <div className="md:hidden flex flex-col items-center gap-4 pb-5 text-lg bg-[#ff66c4]">
+                    <Link to="/Pets" onClick={() => setOpen(false)}>Adote</Link>
+                    <Link to="/Help" onClick={() => setOpen(false)}>Como Ajudar</Link>
+                    <Link to="/LearnMore" onClick={() => setOpen(false)}>Saiba Mais</Link>
+                </div>
+            )}
+
         </div>
-        </>
     )
 }
--- Front-end/src/pages/mainPage.jsx
+++ Front-end/src/pages/mainPage.jsx
@@ -4,143 +4,137 @@ import { Header } from "../components/header";
 import { useNavigate } from "react-router-dom";
 
 export const MainPage = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-   const images = ["/dogo_teste.png", "../public/segundo_dog.png", "/dogo_teste.png", "../public/segundo_dog.png"];
-   const [current, setCurrent] = useState(0);
+    const images = ["/dogo_teste.png", "/segundo_dog.png", "/dogo_teste.png", "/segundo_dog.png"];
+    const [current, setCurrent] = useState(0);
 
-   const nextSlide = () => {
-       setCurrent((prev) => (prev + 1) % images.length);
-   };
+    const nextSlide = () => {
+        setCurrent((prev) => (prev + 1) % images.length);
+    };
 
-   const prevSlide = () => {
-       setCurrent((prev) =>
-           prev === 0 ? images.length - 1 : prev - 1
-       );
-   };
+    useEffect(() => {
+        const interval = setInterval(nextSlide, 3000);
+        return () => clearInterval(interval);
+    }, []);
 
-   useEffect(() => {
-       const interval = setInterval(nextSlide, 3000);
-       return () => clearInterval(interval);
-   }, []);
+    return (
+        <>
+            <Header />
 
-   return (
-       <>
-       {/* Header */}
-       <Header/>
+            {/* First Section */}
+            <div className="w-full min-h-[70vh] bg-[#ff66c4] relative overflow-hidden flex flex-col md:flex-row items-center justify-center md:justify-between px-5 md:px-10">
+                <section className="z-10 text-center md:text-left md:ml-[5%]">
+                    <h1 className="text-5xl md:text-7xl lg:text-9xl text-white">
+                        Me <br/>Adota
+                    </h1>
 
-       {/* Hero */}
-       <div className="w-full h-[65vh] bg-[#ff66c4] relative overflow-hidden flex items-center justify-between px-10">
-           <section className="ml-[5%] z-10">
-               <h1 className="text-9xl text-white">Me <br/>Adota</h1>
-               <button onClick={() => navigate("/Pets")} className="w-[15vw] h-[8vh] bg-[#0097b2] text-white rounded-2xl text-3xl p-[1%] mt-[10%] border-[#0097b2] hover:bg-[#33AEC4]">
-                   Adotar!
-               </button>
-           </section> 
+                    <button
+                        onClick={() => navigate("/Pets")}
+                        className="w-[60vw] md:w-[15vw] h-[6vh] md:h-[8vh] bg-[#0097b2] text-white rounded-2xl text-xl md:text-3xl mt-5 md:mt-[10%] hover:bg-[#33AEC4]">
+                        Adotar!
+                    </button>
+                </section>
 
-           <div className="h-full w-[40%] bg-[#ffef63] rounded-full mt-[10%] relative z-0"></div>
+                <div className="absolute right-[-10%] bottom-[-10%] w-[120vw] h-[40vh] md:w-[40%] md:h-full bg-[#ffef63] rounded-full z-0"></div>
 
-           {/* cachorro */}
-           <img
-               src="/Dog Pirata.png"
-               alt=""
-               className="absolute right-[5%] bottom-0 w-[35%] z-20"
-           />
-       </div>
+                <img src="/Dog Pirata.png" alt="" className=" relative md:absolute md:right-[5%] md:bottom-0 w-[70%] md:w-[35%] mt-5 md:mt-0 z-20 "/>
+            </div>
 
-       {/* Segunda seção */}
-       <div className="bg-white w-full h-[80vh] flex justify-center items-center px-10 gap-[15%]">
-          
-           {/* Card Conheça mais */}
-           <section>
-               <div className="bg-[#0097b2] text-white text-3xl w-[30vw] h-[10vh] flex items-center justify-center rounded-3xl rounded-b-none">
-                   <h1>Quem Somos?</h1>
-               </div>
+            {/* Second Section */}
+            <div className="bg-white w-full min-h-[80vh] flex flex-col md:flex-row justify-center items-center px-5 md:px-10 gap-10 md:gap-[15%]">
 
-               <div className="border-2 border-[#0097b2] text-2xl w-[30vw] h-[40vh] flex flex-col items-center justify-center rounded-3xl rounded-t-none">
-                   <h1 className="text-3xl">Bom pra Cachorro!</h1>
+                {/* Left Card */}
+                <section className="w-[90vw] md:w-[30vw]">
+                    <div className="bg-[#0097b2] text-white text-xl md:text-3xl  w-full h-[8vh] flex items-center justify-center rounded-3xl rounded-b-none">
+                        <h1>Quem Somos?</h1>
+                    </div>
 
-                   <h1 className="m-[7%] text-center">
-                       Somos uma ONG de Curitiba que cuida de mais de 20 animais disponíveis para adoção!
-                   </h1>
+                    <div className="border-2 border-[#0097b2] text-lg md:text-2xl w-full min-h-[40vh] flex flex-col items-center justify-center rounded-3xl rounded-t-none p-5">
+                        <h1 className="text-2xl md:text-3xl">Bom pra Cachorro!</h1>
 
-                   <button onClick={() => navigate("LearnMore")} className="bg-[#ff66c4] hover:bg-[#ff85d1] p-[3%] text-white rounded-2xl">Saiba mais </button>
-               </div>
-           </section>       
+                        <p className="my-5 text-center">
+                            Somos uma ONG de Curitiba que cuida de mais de 20 animais disponíveis para adoção!
+                        </p>
 
-           {/* CARROSSEL */}
-           <section className="w-[23%]">
-               <div className="relative w-full overflow-hidden rounded-2xl">
-                  
-                   {/* Slides */}
-                   <div
-                       className="flex transition-transform duration-500"
-                       style={{ transform: `translateX(-${current * 100}%)` }}
-                   >
-                       {images.map((img, index) => (
-                           <img
-                               key={index}
-                               src={img}
-                               className="w-full shrink-0"
-                               alt=""
-                           />
-                       ))}
-                   </div>
-               </div>
+                        <button onClick={() => navigate("LearnMore")} className="bg-[#ff66c4] hover:bg-[#ff85d1] px-6 py-2 text-white rounded-2xl">
+                            Saiba mais
+                        </button>
+                    </div>
+                </section>
 
-               {/* BOLINHAS */}
-               <div className="flex justify-center gap-2 mt-4">
-                   {images.map((_, index) => (
-                       <div
-                           key={index}
-                           onClick={() => setCurrent(index)}
-                           className={`w-3 h-3 rounded-full cursor-pointer transition-all duration-300
-                               ${current === index
-                                   ? "bg-[#ff66c4] scale-125"
-                                   : "bg-gray-300 hover:bg-gray-400"}
-                           `}
-                       />
-                   ))}
-               </div>
-           </section>
-       </div>
+                {/* Carousel */}
+                <section className="w-[80vw] md:w-[23%]">
+                    <div className="relative w-full overflow-hidden rounded-2xl">
+                        <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${current * 100}%)` }}>
+                            {images.map((img, index) => (
+                                <img key={index} src={img} className="w-full shrink-0" />
+                            ))}
+                        </div>
+                    </div>
 
-       {/* Terceira sessão */}
-       <div className="bg-[#0097b2] w-full h-[80vh] flex justify-center items-center px-10 gap-[6%]">
-            <section>
-                <img src="../public/polaroid_dog.png" alt="" className="w-[90%]"/>
-            </section>
+                    <div className="flex justify-center gap-2 mt-4">
+                        {images.map((_, index) => (
+                            <div
+                                key={index}
+                                onClick={() => setCurrent(index)}
+                                className={`w-3 h-3 rounded-full cursor-pointer
+                                ${current === index ? "bg-[#ff66c4] scale-125" : "bg-gray-300"}`}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </div>
 
-            <section className="flex flex-col gap-6">
-                <h1 className="text-5xl text-white">Saiba como ajudar a ONG</h1>
-                <h1 className="text-2xl text-white">Se voce ama os animais tanto quanto a gente, saiba que pode fazer a diferença!</h1>
-                <h1 className="text-2xl text-white">Com a sua contribuição, conseguimos oferecer alimentacão, cuidados veterinários e muito carinho para os animaiszinhos.</h1>
-                
-                <button onClick={() => navigate("/Help")} className="self-start p-[3%] w-[15vw] text-[#0097b2] text-3xl rounded-4xl bg-white">Contribua!</button>
-            </section>
-       </div>
-    
-        {/* Quarta Secao */}
-       <div className="bg-white w-full h-[80vh] flex justify-center items-center">
-        <section className="w-[80%] h-[55vh] bg-[#ffef63] rounded-4xl flex relative">
-        
-            <section className="flex flex-col justify-center gap-6 ml-[5%] w-[50%]">
-                <h1 className="text-5xl">Violencia contra o animal? Denuncie!</h1>
-                
-                <hr className="border-t-2 border-black w-full" />
-                
-                <h1 className="text-2xl">
-                    Caso presencie qualquer tipo de violencia animal, NÃO SEJA CÚMPLICE!
-                    Disque 181 ou 156 para denunciar. Eles não podem pedir ajuda, mas você pode!
-                </h1>
-            </section>
+            {/* Third Section */}
+            <div className="bg-[#0097b2] w-full min-h-[80vh] flex flex-col md:flex-row justify-center items-center px-5 md:px-10 gap-10">
 
-            <img src="../public/heroi_dogo.png" alt="" className="w-[55%] absolute -right-30  bottom-0 -top-20"/>
-        
-        </section>
-        </div>
+                <section>
+                    <img src="/polaroid_dog.png" className="w-[70%] md:w-[90%]" />
+                </section>
 
-       <Footer/>
-       </>
-   );
+                <section className="flex flex-col gap-6 text-center md:text-left">
+                    <h1 className="text-3xl md:text-5xl text-white">
+                        Saiba como ajudar a ONG
+                    </h1>
+
+                    <p className="text-lg md:text-2xl text-white">
+                        Se você ama os animais tanto quanto a gente, saiba que pode fazer a diferença!
+                    </p>
+
+                    <p className="text-lg md:text-2xl text-white">
+                        Com a sua contribuição, conseguimos oferecer alimentação, cuidados veterinários e muito carinho.
+                    </p>
+
+                    <button onClick={() => navigate("/Help")} className="self-center md:self-start px-6 py-3 w-[60vw] md:w-[15vw] text-[#0097b2] text-xl md:text-3xl rounded-4xl bg-white">
+                        Contribua!
+                    </button>
+                </section>
+            </div>
+
+            {/* Fouth Section */}
+            <div className="bg-white w-full min-h-[80vh] flex justify-center items-center px-5">
+
+                <section className="w-full md:w-[80%] min-h-[55vh] bg-[#ffef63] rounded-4xl flex flex-col md:flex-row relative p-5">
+
+                    <section className="flex flex-col justify-center gap-6 w-full md:w-[50%]">
+                        <h1 className="text-3xl md:text-5xl">
+                            Violência contra o animal? Denuncie!
+                        </h1>
+
+                        <hr className="border-t-2 border-black w-full" />
+
+                        <p className="text-lg md:text-2xl">
+                            Caso presencie qualquer tipo de violência animal, NÃO SEJA CÚMPLICE!
+                            Disque 181 ou 156 para denunciar.
+                        </p>
+                    </section>
+
+                    <img src="/heroi_dogo.png" className="relative md:absolute md:-right-30 md:bottom-0 md:-top-20 w-[80%] md:w-[55%] mx-auto" />
+                </section>
+            </div>
+
+            <Footer />
+        </>
+    );
 };
