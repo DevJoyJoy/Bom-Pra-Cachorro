c6cfbfa89f56377abe4e5c69b06f1b68939b7c28
taykas <thaynaschaeffer@gmail.com>
2026-04-10 08:57:09 -0300

    Testing responsive

--- Front-end/src/pages/mainPage.jsx
+++ Front-end/src/pages/mainPage.jsx
@@ -23,168 +23,129 @@ export const MainPage = () => {
         <>
             <Header open={menuOpen} setOpen={setMenuOpen} />
 
-            {/* First Section */}
-            <div className="w-full h-[70vh] bg-[#ff66c4] flex items-center
-                md:h-[50vh]">
+            {/* FIRST SECTION */}
+            <div className="w-full h-[66.2vh] bg-[#ff66c4] flex flex-col md:flex-row items-center justify-between px-6 py-10">
 
-                    {/* Conteudo "Me adota" */}
-                    <section className="flex flex-col gap-6 ml-[9%]
-                        md:ml-[5%] md:self-center
-                        sm:self-start sm:text-center sm:ml-[15%] sm:gap-0 sm:mt-[4%]">
+                <section className="flex flex-col gap-6 max-w-lg text-center md:text-left">
+                    <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-white font-bold">
+                        Me Adota
+                    </h1>
 
-                        <section className="w-[20%]
-                            md:w-80 md:text-left 
-                            sm:w-110">
-                            <h1 className="text-9xl text-white
-                                sm:text-8xl">
-                                Me Adota</h1>
-                        </section>
-
-                        <button
-                            onClick={() => navigate("/Pets")}
-                            className="w-[10vw] p-[2%] bg-[#0097b2] text-white rounded-4xl text-3xl mt-5 hover:bg-[#33AEC4]
-                                md:self-start
-                                sm:w-[45%] sm:self-center">
-                                Adotar!
-                        </button>
-                    </section>
-
-                   <div className={`absolute right-[15%] z-20
-                        md:right-0
-                        ${menuOpen ? "top-[20%] sm:top-[55.2%] md:top-[1%]" : "top-[3.5%] sm:top-[39.2%] md:top-[19%]"}
-                    `}>
-                        <img 
-                            src="/dog pirata certa.png" alt="" className="w-230 
-                                sm:w-110"
-                        />
-                    </div>
-                </div>  
-
-
-                {/* Second Section */}
-                <div className="w-full h-[85vh] bg-white flex items-center justify-center gap-[10%] z-10 mt-[3%]
-                    md:flex-row md:h-[75vh]
-                    sm:flex-col sm:gap-10 sm:h-[140vh]">
-                    
-                    <section className="w-[30vw] flex flex-col
-                        md:mt-0 md:w-[40%]
-                        sm:mt-[10%] sm:w-[70%]">
-                        <div className="bg-[#0097b2] text-white text-4xl w-full h-[8vh] flex items-center justify-center rounded-3xl rounded-b-none">
-                            <h1>Quem Somos?</h1>
-                        </div>
-
-                        <div className="border-2 border-[#0097b2] text-lg w-full min-h-[40vh] flex flex-col items-center justify-center rounded-3xl rounded-t-none p-5 gap-10
-                            sm:gap-5">
-                            <h1 className="
-                                md:text-2xl
-                                sm:text-3xl">
-                                Bom pra Cachorro!</h1>
-
-                            <p className="w-[70%] text-center text-2xl
-                                md:text-[20px] md:w-[120%]
-                                sm:text-[24px] sm:w-[80%]">
-                                Somos uma ONG de Curitiba que cuida de mais de 20 animais disponíveis para adoção!
-                            </p>
-
-                            <button onClick={() => navigate("LearnMore")} className="bg-[#ff66c4] hover:bg-[#ff85d1] w-[35%] text-white rounded-2xl p-[2%] text-3xl
-                                md:w-[80%]
-                                sm:w-[50%]">
-                                Saiba mais
-                            </button>
-                        </div>
-                    </section>
-
-                    <section className="w-[60vw] z-10
-                        md:w-[40vw]
-                        sm:w-[70vw]">
-                        <div className="relative w-full overflow-hidden rounded-2xl">
-                            <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${current * 100}%)` }}>
-                                {images.map((img, index) => (
-                                    <img key={index} src={img} className="w-full shrink-0" />
-                                ))}
-                            </div>
-                        </div>
-
-                        <div className="flex justify-center gap-2 mt-4">
-                            {images.map((_, index) => (
-                                <div
-                                    key={index}
-                                    onClick={() => setCurrent(index)}
-                                    className={`w-3 h-3 rounded-full cursor-pointer
-                                    ${current === index ? "bg-[#ff66c4] scale-125" : "bg-gray-300"}`}
-                                />
-                            ))}
-                        </div>
-                    </section>
-                </div>
-
-            {/* Third Section */}
-            <div className="bg-[#0097b2] w-full h-[80vh] flex flex-col justify-center items-center px-5 gap-10
-                md:flex-row md:h-[75vh]
-                sm:h-screen">
-
-                <section className="flex items-center justify-center">
-                    <img src="/polaroid_dog.png" className="w-[70%] 
-                        md:w-[110vw]
-                        sm:self-center sm:w-[80%]"/>
+                    <button
+                        onClick={() => navigate("/Pets")}
+                        className="px-6 py-3 bg-[#0097b2] text-white rounded-full text-xl hover:bg-[#33AEC4]
+                        w-full sm:w-[60%] md:w-fit mx-auto md:mx-0">
+                        Adotar!
+                    </button>
                 </section>
 
-                <section className="flex flex-col gap-6 text-center 
-                    md:text-left
-                    sm:gap-4">
-                    <h1 className="text-3xl text-white
-                        md:text-3xl">
+                <img 
+                    src="/dog pirata certa.png"
+                    className="w-80 sm:w-70 md:w-[320px] lg:w-95 mt-8 md:mt-0"
+                />
+            </div>  
+
+            {/* SECOND SECTION */}
+            <div className="w-full bg-white flex flex-col md:flex-row items-center justify-center gap-10 px-6 py-10">
+                
+                <section className="w-full max-w-md">
+                    <div className="bg-[#0097b2] text-white text-xl md:text-2xl h-14 flex items-center justify-center rounded-t-2xl">
+                        Quem Somos?
+                    </div>
+
+                    <div className="border-2 border-[#0097b2] p-6 rounded-b-2xl flex flex-col gap-6 text-center">
+                        <h1 className="text-xl md:text-2xl">
+                            Bom pra Cachorro!
+                        </h1>
+
+                        <p className="text-base md:text-lg">
+                            Somos uma ONG de Curitiba que cuida de mais de 20 animais disponíveis para adoção!
+                        </p>
+
+                        <button 
+                            onClick={() => navigate("LearnMore")}
+                            className="bg-[#ff66c4] text-white rounded-xl py-2 px-4 hover:bg-[#ff85d1]">
+                            Saiba mais
+                        </button>
+                    </div>
+                </section>
+
+                <section className="w-full max-w-lg">
+                    <div className="overflow-hidden rounded-2xl">
+                        <div 
+                            className="flex transition-transform duration-500"
+                            style={{ transform: `translateX(-${current * 100}%)` }}
+                        >
+                            {images.map((img, index) => (
+                                <img key={index} src={img} className="w-full shrink-0" />
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center gap-2 mt-4">
+                        {images.map((_, index) => (
+                            <div
+                                key={index}
+                                onClick={() => setCurrent(index)}
+                                className={`w-3 h-3 rounded-full cursor-pointer
+                                ${current === index ? "bg-[#ff66c4] scale-125" : "bg-gray-300"}`}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </div>
+
+            {/* THIRD SECTION */}
+            <div className="bg-[#0097b2] w-full flex flex-col md:flex-row items-center justify-center px-6 py-10 gap-10">
+
+                <img 
+                    src="/polaroid_dog.png" 
+                    className="w-[70%] max-w-sm"
+                />
+
+                <section className="flex flex-col gap-4 items-center justify-center text-center md:text-left max-w-md">
+                    <h1 className="text-xl md:text-2xl text-white">
                         Saiba como ajudar a ONG
                     </h1>
 
-                    <p className="text-lg text-white
-                        md:text-xl">
+                    <p className="text-white">
                         Se você ama os animais tanto quanto a gente, saiba que pode fazer a diferença!
                     </p>
 
-                    <p className="text-lg text-white
-                        md:text-xl">
+                    <p className="text-white">
                         Com a sua contribuição, conseguimos oferecer alimentação, cuidados veterinários e muito carinho.
                     </p>
 
-                    <button onClick={() => navigate("/Help")} className="self-center px-6 py-3 w-[60vw] text-[#0097b2] text-xl rounded-4xl bg-white
-                        md:self-start md:w-[35vw] md:text-xl
-                        sm:w-[45vw]">
+                    <button 
+                        onClick={() => navigate("/Help")}
+                        className="px-6 py-3 bg-white text-[#0097b2] rounded-full w-[80%] md:w-fill">
                         Saiba como Contribuir!
                     </button>
                 </section>
             </div>
 
-            {/* Fouth Section */}
-            <div className="bg-white w-full min-h-[80vh] flex justify-center items-center px-5
-                md:h-[75vh]
-                sm:h-screen">
+            {/* FOURTH SECTION */}
+            <div className="bg-white w-full flex justify-center px-6 py-10">
 
-                <section className="w-full h-[55vh] bg-[#ffef63] rounded-4xl flex flex-col relative p-5
-                    md:w-[90%] md:h-[55vh] md:flex-row
-                    sm:text-center">
+                <section className="w-full max-w-5xl bg-[#ffef63] rounded-3xl flex flex-col md:flex-row items-center p-6 gap-6">
 
-                    <section className="flex flex-col justify-center gap-6 w-full 
-                        md:w-[65%]
-                        sm:mt-[5%]">
-                        <h1 className="text-3xl 
-                            md:text-3xl
-                            sm:text-3xl">
+                    <div className="flex flex-col gap-4 text-center md:text-left">
+                        <h1 className="text-xl md:text-2xl">
                             Violência contra o animal? Denuncie!
                         </h1>
 
-                        <hr className="border-t-2 border-black w-full" />
+                        <hr className="border-black" />
 
-                        <p className="text-lg 
-                            md:text-xl
-                            sm:text-xl">
+                        <p className="text-base md:text-lg">
                             Caso presencie qualquer tipo de violência animal, NÃO SEJA CÚMPLICE!
                             Disque 181 ou 156 para denunciar.
                         </p>
-                    </section>
+                    </div>
 
-                    <img src="/heroi_dogo.png" className="relative w-[80%] mx-auto
-                        md:absolute md:-right-30 md:bottom-0 md:top-20 md:w-[70%]" />
+                    <img 
+                        src="/heroi_dogo.png"
+                        className="w-[70%] max-w-xs"
+                    />
                 </section>
             </div>
 
