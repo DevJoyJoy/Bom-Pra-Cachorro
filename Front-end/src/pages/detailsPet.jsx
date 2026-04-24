65b840801d4cf637bf0cd586c9f8d7bcb2ae8f26
devYuno <annykessy20@gmail,com>
2026-04-23 21:54:42 -0300

    FIX: layout do detailsPet parcialmente arrumado e active dos botoes de findAnimals adicionado

--- Front-end/src/pages/detailsPet.jsx
+++ Front-end/src/pages/detailsPet.jsx
@@ -9,9 +9,9 @@ export const DetailsPetPage = () => {
         <div id="screen" className="w-full min-h-screen flex flex-col items-center">
             <Header open={menuOpen} setOpen={setMenuOpen}></Header>
 
-            <div id="container" className="w-[80%] h-100% flex-1 flex flex-col md:mt-6 md:flex-row">
+            <div id="container" className="w-[85%] md:mt-6 flex flex-1 flex-col md:flex-row md:gap-8">
                 {/* Titulo principal / Nome do pet */}
-                <div className="order-1 md:order-2 flex flex-col h-max flex-1">
+                <div className=" flex flex-col h-max md:hidden ">
                     <div id="name-pet" className="flex flex-row items-center">
                         <img src="../../public/patinha.png" className="h-6 mr-4 mt-3"></img>
                         <h1 className="mt-4 text-2xl">Rogerinho</h1>
@@ -19,15 +19,26 @@ export const DetailsPetPage = () => {
                     <div className="my-3 h-0.5 w-full bg-[#9cd7e1]"></div>
                 </div>
                 {/* Coluna 1 da pagina md / coluna normal em mobile */}
-                <div className="order-2 md:order-1 flex items-center flex-col md:w-80 md:mr-5">
-                    <img src="../../public/exampleDog.jpg" className="w-full md:h-80 m-3 rounded-2xl border-5 border-[#0097b2]"></img>
+                <div className="order-1 flex items-center flex-col md:w-70 md:mr-5 lg:w-80">
+                    <img src="../../public/exampleDog.jpg" className="w-full m-3 rounded-2xl border-5 border-[#0097b2]"></img>
                     <div className="bg-[#9cd7e1] rounded-2xl my-4 flex w-full aspect-square  ">
-                        
+
                     </div>
-                    <button className="bg-[#0097b2] w-full rounded-xl text-white py-2 md:w-80">Quero adotar</button>
+                    <button className="bg-[#0097b2] w-full rounded-xl text-white py-2 ">Quero adotar</button>
+                </div>
+                <div className="h-max order-2 flex-1">
+                    <div className="hidden md:flex ">
+                        <div className="h-max flex flex-col flex-1">
+                            <div id="name-pet" className="flex flex-row items-center">
+                                <img src="../../public/patinha.png" className="h-6 mr-4 mt-3"></img>
+                                <h1 className="mt-4 text-2xl">Rogerinho</h1>
+                            </div>
+                            <div className="my-3 h-0.5 w-full bg-[#9cd7e1]"></div>
+                        </div>
+                    </div>
+                    <div className="w-full bg-[#9cd7e1] h-300"></div>
                 </div>
 
-                
 
 
 
--- Front-end/src/pages/findAnimals.jsx
+++ Front-end/src/pages/findAnimals.jsx
@@ -1,15 +1,17 @@
 import { Header } from "../components/header"
 import { Footer } from "../components/footer"
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const FindAnimals = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     return (
         <>
             <div className="w-full min-h-screen flex flex-col">
 
-                <Header open={menuOpen} setOpen={setMenuOpen}/>
+                <Header open={menuOpen} setOpen={setMenuOpen} />
 
                 {/* Box title */}
                 <div className="w-[90%] m-5">
@@ -20,56 +22,60 @@ export const FindAnimals = () => {
                 <div className="w-full flex flex-1 items-start justify-center">
                     <div className="w-full max-w-4xl mx-auto p-4 md:px-9 lg:px-0 flex flex-wrap items-start justify-center gap-9 sm:gap-10">
                         {/* Example of cards for the pets*/}
-                        <div className="h-[30%] w-[40%] p-0 sm:w-[27%] lg:h-[25%] lg:w-[30%] lg:p-2">
+                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" onClick={() => {navigate('/teste')}}> {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
-                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105" />
+                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
                                 <span className="text-lg">Doguinho</span> <br></br>
                                 <span className="text-lg">3 anos</span>
                             </div>
                         </div>
-                        <div className="h-[30%] w-[40%] p-0 sm:w-[27%] lg:h-[25%] lg:w-[30%] lg:p-2">
+                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
-                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105" />
+                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
                                 <span className="text-lg">Doguinho</span> <br></br>
                                 <span className="text-lg">3 anos</span>
                             </div>
-                        </div><div className="h-[30%] w-[40%] p-0 sm:w-[27%] lg:h-[25%] lg:w-[30%] lg:p-2">
+                        </div>
+                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
-                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105" />
+                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
                                 <span className="text-lg">Doguinho</span> <br></br>
                                 <span className="text-lg">3 anos</span>
                             </div>
-                        </div><div className="h-[30%] w-[40%] p-0 sm:w-[27%] lg:h-[25%] lg:w-[30%] lg:p-2">
+                        </div>
+                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
-                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105" />
+                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
                                 <span className="text-lg">Doguinho</span> <br></br>
                                 <span className="text-lg">3 anos</span>
                             </div>
-                        </div><div className="h-[30%] w-[40%] p-0 sm:w-[27%] lg:h-[25%] lg:w-[30%] lg:p-2">
+                        </div>
+                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
-                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105" />
+                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
                                 <span className="text-lg">Doguinho</span> <br></br>
                                 <span className="text-lg">3 anos</span>
                             </div>
-                        </div><div className="h-[30%] w-[40%] p-0 sm:w-[27%] lg:h-[25%] lg:w-[30%] lg:p-2">
+                        </div>
+                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
-                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105" />
+                                <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
                                 <span className="text-lg">Doguinho</span> <br></br>
