dd322405a69101ad97f7d5a89b930d4bc861d918
devYuno <annykessy20@gmail,com>
2026-04-24 21:12:11 -0300

    FEAT: container para dados do pet

--- Front-end/src/pages/detailsPet.jsx
+++ Front-end/src/pages/detailsPet.jsx
@@ -21,8 +21,27 @@ export const DetailsPetPage = () => {
                 {/* Coluna 1 da pagina md / coluna normal em mobile */}
                 <div className="order-1 flex items-center flex-col md:w-70 md:mr-5 lg:w-80">
                     <img src="../../public/exampleDog.jpg" className="w-full m-3 rounded-2xl border-5 border-[#0097b2]"></img>
-                    <div className="bg-[#9cd7e1] rounded-2xl my-4 flex w-full aspect-square  ">
-
+                    <div className="bg-[#daeef5] grid grid-cols-2 grid-rows-2 rounded-2xl my-4 w-full aspect-square  ">
+                        <div className="flex flex-col items-center justify-center p-3">
+                            <img src="../../public/icon-birthday.png" className="w-18 h-max"></img>
+                            <label>Idade:</label>
+                            <label>2 anos</label>
+                        </div>
+                        <div className="flex flex-col items-center justify-center p-3">
+                            <img src="../../public/icon-regua.png" className="w-18 h-max"></img>
+                            <label>Porte:</label>
+                            <label>Médio</label>
+                        </div>
+                        <div className="flex flex-col items-center justify-center p-3">
+                            <img src="../../public/icon-dna.png" className="w-18 h-max"></img>
+                            <label>Raça:</label>
+                            <label>Indefinida</label>
+                        </div>
+                        <div className="flex flex-col items-center justify-center p-3">
+                            <img src="../../public/icon-sex.png" className="w-18 h-max"></img>
+                            <label>Sexo:</label>
+                            <label>Macho</label>
+                        </div>
                     </div>
                     <button className="bg-[#0097b2] w-full rounded-xl text-white py-2 ">Quero adotar</button>
                 </div>
--- Front-end/src/pages/findAnimals.jsx
+++ Front-end/src/pages/findAnimals.jsx
@@ -20,11 +20,11 @@ export const FindAnimals = () => {
 
                 {/* Box for all the animals */}
                 <div className="w-full flex flex-1 items-start justify-center">
-                    <div className="w-full max-w-4xl mx-auto p-4 md:px-9 lg:px-0 flex flex-wrap items-start justify-center gap-9 sm:gap-10">
+                    <div className="w-full max-w-4xl mx-auto p-4 md:px-9 lg:px-0 flex flex-wrap items-start justify-center gap-5 sm:gap-10">
                         {/* Example of cards for the pets*/}
-                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" onClick={() => {navigate('/teste')}}> {/*onClick={() => {navigate('/teste')}}*/}
+                        <div className="h-[30%] w-[90%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" onClick={() => {navigate('/teste')}}> {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
-                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
+                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-10" />
                                 <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
@@ -32,9 +32,9 @@ export const FindAnimals = () => {
                                 <span className="text-lg">3 anos</span>
                             </div>
                         </div>
-                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
+                        <div className="h-[30%] w-[90%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
-                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
+                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-10" />
                                 <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
@@ -42,9 +42,9 @@ export const FindAnimals = () => {
                                 <span className="text-lg">3 anos</span>
                             </div>
                         </div>
-                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
+                        <div className="h-[30%] w-[90%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
-                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
+                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-10" />
                                 <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
@@ -52,9 +52,9 @@ export const FindAnimals = () => {
                                 <span className="text-lg">3 anos</span>
                             </div>
                         </div>
-                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
+                        <div className="h-[30%] w-[90%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
-                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
+                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-10" />
                                 <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
@@ -62,9 +62,9 @@ export const FindAnimals = () => {
                                 <span className="text-lg">3 anos</span>
                             </div>
                         </div>
-                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
+                        <div className="h-[30%] w-[40%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
-                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
+                                <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-9" />
                                 <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
                             </div>
                             <div className="pl-3">
@@ -72,7 +72,7 @@ export const FindAnimals = () => {
                                 <span className="text-lg">3 anos</span>
                             </div>
                         </div>
-                        <div className="h-[30%] w-[45%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
+                        <div className="h-[30%] w-[40%] p-2 sm:w-[27%] lg:h-[25%] lg:w-[30%] rounded-2xl cursor-pointer transition-all duration-150 hover:-translate-y-1 hover:shadow-lg active:scale-95 active:translate-y-1 active:shadow-sm" > {/*onClick={() => {navigate('/teste')}}*/}
                             <div className="relative w-full h-[60%] group">
                                 <img src="../fundo_grid_pet.png" className="w-full rounded-2xl transition-transform duration-300 group-hover:-rotate-6" />
                                 <img src="../exampleDog.jpg" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%] rounded-2xl transition-transform duration-300 group-hover:scale-105 " />
