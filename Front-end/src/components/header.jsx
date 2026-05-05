be34994d54d945dc26c8c290ffd565026dd7fb43
devYuno <annykessy20@gmail,com>
2026-05-04 21:24:29 -0300

    FEAT: botão secreto no header para pagina de login

--- Front-end/src/components/header.jsx
+++ Front-end/src/components/header.jsx
@@ -4,31 +4,35 @@ import { useState } from "react";
 export const Header = ({ open, setOpen }) => {
     const navigate = useNavigate();
 
-    return(
+    return (
         <div className="w-full bg-[#ff66c4] text-white">
+            <div className="h-18 flex items-center pr-5 md:pr-10">
+                {/* O botão tem double click para levar para pagina secreta de login */}
+                <button onDoubleClick={() => navigate('/login')} className="w-7 h-full"></button>
+                <div className="flex justify-between w-full items-center">
 
-            <div className="h-18 flex justify-between items-center px-5 md:px-10">
-                <section 
-                    className="flex gap-2 items-center cursor-pointer"
-                    onClick={() => navigate("/")}
-                >
-                    <img src="/icon paw.png" className="w-12   " />
-                    <h1 className="text-lg md:text-2xl lg:text-3xl font-arturo">
-                        Bom pra Cachorro
-                    </h1>
-                </section>
+                    <section
+                        className="flex gap-2 items-center cursor-pointer"
+                        onClick={() => navigate("/")}
+                    >
+                        <img src="/icon paw.png" className="w-12   " />
+                        <h1 className="text-lg md:text-2xl lg:text-3xl font-arturo">
+                            Bom pra Cachorro
+                        </h1>
+                    </section>
 
-                {/* MENU DESKTOP */}
-                <section className="hidden md:flex gap-6 text-lg">
-                    <Link to="/Pets">Adote</Link>
-                    <Link to="/Help">Como Ajudar</Link>
-                    <Link to="/WhoWeAre">Saiba Mais</Link>
-                </section>
+                    {/* MENU DESKTOP */}
+                    <section className="hidden md:flex gap-6 text-lg">
+                        <Link to="/Pets">Adote</Link>
+                        <Link to="/Help">Como Ajudar</Link>
+                        <Link to="/WhoWeAre">Saiba Mais</Link>
+                    </section>
 
-                {/* BOTÃO MOBILE */}
-                <div className="md:hidden text-3xl cursor-pointer"onClick={() => setOpen(!open)}
-                >
-                    ☰
+                    {/* BOTÃO MOBILE */}
+                    <div className="md:hidden text-3xl cursor-pointer" onClick={() => setOpen(!open)}
+                    >
+                        ☰
+                    </div>
                 </div>
             </div>
 
