91340cb8fe17899576ee3e304222bfacf3a0a09a
“DevJoyJoy” <joycenascimento2007@gmail.com>
2026-05-07 16:54:34 -0300

    Made some adjustments on edit/delete page style

--- Front-end/src/pages/availablePets.jsx
+++ Front-end/src/pages/availablePets.jsx
@@ -14,7 +14,7 @@ export const AvaliablePets = () => {
         
         {/* White box */}
         <div className="flex flex-col items-center justify-start p-3
-        w-auto h-[90vh] max-h-[95vh] 
+        w-auto h-[88vh] max-h-[95vh] 
         lg:pl-10
         ">
             <h1 className="pt-3 lg:pb-3 font-bold breeSerif
@@ -23,7 +23,8 @@ export const AvaliablePets = () => {
             lg:text-3xl">
                 Editar animais disponíveis
             </h1>
-
+            
+            {/* Animal box*/}
             <div className="flex flex-col mt-3 gap-4 items-center
             w-full h-full overflow-y-scroll">
                 <div className="bg-[#e7e7e7] rounded-2xl gap-2 shadow-xl flex flex-row
@@ -34,15 +35,16 @@ export const AvaliablePets = () => {
                         <p className="lg:text-xl">Idade: </p>
                         <p className="lg:text-xl">Raça: </p>
                     </div>
+                    {/* Box for buttons */}
                     <div className="flex gap-1 ml-auto self-start p-2">
-                        <div className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%]
+                        <button className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#5f5f5f]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">!</div>
-                        <div className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%]
+                        lg:w-[3vw]">!</button>
+                        <button className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#661616]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">X</div>
+                        lg:w-[3vw]">X</button>
                     </div>
                 </div>
 
@@ -55,15 +57,16 @@ export const AvaliablePets = () => {
                         <p className="lg:text-xl">Idade: </p>
                         <p className="lg:text-xl">Raça: </p>
                     </div>
+                    {/* Box for buttons */}
                     <div className="flex gap-1 ml-auto self-start p-2">
-                        <div className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%]
+                        <button className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#5f5f5f]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">!</div>
-                        <div className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%]
+                        lg:w-[3vw]">!</button>
+                        <button className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#661616]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">X</div>
+                        lg:w-[3vw]">X</button>
                     </div>
                 </div>
 
@@ -75,15 +78,16 @@ export const AvaliablePets = () => {
                         <p className="lg:text-xl">Idade: </p>
                         <p className="lg:text-xl">Raça: </p>
                     </div>
+                    {/* Box for buttons */}
                     <div className="flex gap-1 ml-auto self-start p-2">
-                        <div className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%]
+                        <button className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#5f5f5f]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">!</div>
-                        <div className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%]
+                        lg:w-[3vw]">!</button>
+                        <button className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#661616]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">X</div>
+                        lg:w-[3vw]">X</button>
                     </div>
                 </div>
 
@@ -95,15 +99,16 @@ export const AvaliablePets = () => {
                         <p className="lg:text-xl">Idade: </p>
                         <p className="lg:text-xl">Raça: </p>
                     </div>
+                    {/* Box for buttons */}
                     <div className="flex gap-1 ml-auto self-start p-2">
-                        <div className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%]
+                        <button className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#5f5f5f]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">!</div>
-                        <div className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%]
+                        lg:w-[3vw]">!</button>
+                        <button className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#661616]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">X</div>
+                        lg:w-[3vw]">X</button>
                     </div>
                 </div>
 
@@ -115,15 +120,16 @@ export const AvaliablePets = () => {
                         <p className="lg:text-xl">Idade: </p>
                         <p className="lg:text-xl">Raça: </p>
                     </div>
+                    {/* Box for buttons */}
                     <div className="flex gap-1 ml-auto self-start p-2">
-                        <div className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%]
+                        <button className="bg-[#868585] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#5f5f5f]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">!</div>
-                        <div className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%]
+                        lg:w-[3vw]">!</button>
+                        <button className="bg-[#972222] text-white flex items-center justify-center p-3 rounded-[30%] cursor-pointer hover:bg-[#661616]
                         w-[5vw] h-[4vh]
                         md:h-[6vh]
-                        lg:w-[3vw]">X</div>
+                        lg:w-[3vw]">X</button>
                     </div>
                 </div>
             </div>
