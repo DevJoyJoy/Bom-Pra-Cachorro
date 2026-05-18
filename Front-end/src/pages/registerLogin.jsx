b81aa77ba9d22d1b92341f428d58a6139d6ece52
Jhenie <highhalma@gmail.com>
2026-05-18 19:50:19 -0300

    handleLogin and handleRegisterPet done

--- Front-end/src/pages/registerLogin.jsx
+++ Front-end/src/pages/registerLogin.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { Header } from "../components/header";
 import { useState } from "react";
+import { auth, db } from "../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 export const RegisterLogin = () => {
   const navigate = useNavigate();
@@ -18,7 +21,7 @@ export const RegisterLogin = () => {
 
       const querySnapshot = await getDocs(q);
       if (querySnapshot.empty) {
-        setResponse("Usuário não encontrado.");
+        alert("Usuário não encontrado.");
         return;
       }
       const userData = querySnapshot.docs[0].data();
@@ -34,50 +37,77 @@ export const RegisterLogin = () => {
   };
 
   return (
-    <div className="flex flex-col
+    <div
+      className="flex flex-col
       h-full w-full
-      lg:h-[91vh] lg:w-full">
+      lg:h-[91vh] lg:w-full"
+    >
       {/* Header */}
       <Header />
 
       {/* Main content box */}
-      <div className="flex
+      <div
+        className="flex
         w-screen h-[91vh] items-center justify-center
-        lg:flex-row">
-          {/* Texts box */}
-          <div className="flex flex-col items-center justify-center 
+        lg:flex-row"
+      >
+        {/* Texts box */}
+        <div
+          className="flex flex-col items-center justify-center 
             lg:h-[91vh] lg:w-[70%]"
-            >
-            <p className="text-3xl lg:text-3xl md:text-5xl">Cadastro de animais:</p>
-            <br />
-            <p className=" text-2xl lg:text-2xl md:text-3xl text-center">Faça login para realizar o cadastro de animais!</p>
-            <br />
-            <br />
+        >
+          <p className="text-3xl lg:text-3xl md:text-5xl">
+            Cadastro de animais:
+          </p>
+          <br />
+          <p className=" text-2xl lg:text-2xl md:text-3xl text-center">
+            Faça login para realizar o cadastro de animais!
+          </p>
+          <br />
+          <br />
 
-            {/* Inputs box */}
-            <div className="flex flex-col">
-              <p className="text-xl lg:text-xl md:text-3xl">Usuário</p>
-              <input type="text" name="" id="" placeholder="Digite seu usuário:" className="bg-[#E7E7E7] p-[0.8vh] rounded-xl
-                lg:w-[18vw] lg:text-xl
-                md:text-2xl"/>
-              <br />
-              <p className="text-xl lg:text-xl md:text-3xl">Senha
-              </p>
-              <input type={show? "text" : "password"} name="" id="" placeholder="Digite sua senha:" className="bg-[#E7E7E7] p-[0.8vh] rounded-xl
-                lg:w-[18vw] lg:text-xl
-                md:text-2xl"/>
-              <button className="md:text-xl" onClick={() => setShow(!show)}> {show ? "Ocultar senha" : "Mostrar senha"}
-              </button>
-              <br />
-              <br />
-              <button onClick={() => navigate('/Register')} className="arturo bg-[#0097b2] text-white rounded-xl cursor-pointer font-bold hover:bg-[#015b6b] p-[1vh] text-2xl">Entrar</button>
-            </div>
+          {/* Inputs box */}
+          <div className="flex flex-col">
+            <p className="text-xl lg:text-xl md:text-3xl">Usuário</p>
+            <input
+              type="text"
+              placeholder="Digite seu usuário:"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="bg-[#E7E7E7] p-[0.8vh] rounded-xl lg:w-[18vw] lg:text-xl md:text-2xl"
+            />
+            <br />
+            <p className="text-xl lg:text-xl md:text-3xl">Senha</p>
+            <input
+              type={show ? "text" : "password"}
+              placeholder="Digite sua senha:"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              className="bg-[#E7E7E7] p-[0.8vh] rounded-xl lg:w-[18vw] lg:text-xl md:text-2xl"
+            />
+            <button className="md:text-xl" onClick={() => setShow(!show)}>
+              {" "}
+              {show ? "Ocultar senha" : "Mostrar senha"}
+            </button>
+            <br />
+            <br />
+            <button
+              onClick={handleLogin}
+              className="arturo bg-[#0097b2] text-white rounded-xl cursor-pointer font-bold hover:bg-[#015b6b] p-[1vh] text-2xl"
+            >
+              Entrar
+            </button>
           </div>
-          {/* Image (only on desktop)*/}
-          <img src="/DogLogin.png" alt="" className="h-0 w-0
+        </div>
+        {/* Image (only on desktop)*/}
+        <img
+          src="/DogLogin.png"
+          alt=""
+          className="h-0 w-0
             md:h-0 md:w-0
             lg:h-[91vh] lg:w-[35%]
-            "/>
+            "
+        />
       </div>
     </div>
   );
